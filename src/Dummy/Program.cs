28b0ab70b88d20f5c68f60d863ddbadb27d646df
Pati <patrickmistlberger@gmail.com>
2024-04-05 00:48:17 +0200

    get all from link
    only for anicloud

--- src/Dummy/Program.cs
+++ src/Dummy/Program.cs
@@ -9,30 +9,107 @@ using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Linq;
 
+
 namespace Dummy
 {
     internal class Program
     {
         static async Task Main(string[] args)
         {
-
             HttpClient client = new HttpClient();
-            HttpResponseMessage response = await client.GetAsync("https://aniworld.to/anime/stream/i-was-reincarnated-as-the-7th-prince-so-i-can-take-my-time-perfecting-my-magical-ability");
-            //string i = await response.Content.ReadAsStringAsync();
-            //var xml = new XmlDocument();
-            //xml.LoadXml(Encoding.Default.GetString(await response.Content.ReadAsByteArrayAsync()));
 
-
-            XmlDocument document = new XmlDocument();
-            using (Stream stream = await response.Content.ReadAsStreamAsync())
-            {
-                using (XmlReader reader = XmlReader.Create(stream))
-                {
-                    document.Load(reader);
-                }
-            }
+            var s = await GetAllFromLink(client, "https://aniworld.to/anime/stream/classroom-of-the-elite");
 
             Console.WriteLine();
         }
+
+        static async Task<XmlDocument> GetXml(HttpClient client, string url)
+        {
+            HttpResponseMessage response = await client.GetAsync(url);
+
+            Sgml.SgmlReader sgml = new Sgml.SgmlReader()
+            {
+                DocType = "HTML",
+                WhitespaceHandling = WhitespaceHandling.All,
+                CaseFolding = Sgml.CaseFolding.ToLower,
+                InputStream = new StreamReader(await response.Content.ReadAsStreamAsync())
+            };
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(sgml);
+            return doc;
+        }
+
+        static async Task<List<Season>> GetAllSeason(HttpClient client, string url)
+        {
+            var xml = await GetXml(client, url);
+
+            const string uri = "https://aniworld.to";
+            var seasonList = xml.SelectNodes("//*[@id='stream']//ul");
+            var getSeasonList = seasonList?.Item(0)?.SelectNodes("*/a");
+
+            var seasons = new List<Season>();
+            foreach (XmlElement s in getSeasonList)
+            {
+                seasons.Add(new Season()
+                {
+                    SeasonTitle = s.InnerText,
+                    SeasonLink = s.GetAttribute("href")
+                });
+            }
+
+            foreach (var season in seasons)
+            {
+                var s = await GetXml(client, uri + season.SeasonLink);
+                var l = s.SelectNodes("//*[@id='stream']//ul");
+                var sl = l?.Item(1)?.SelectNodes("*/a");
+                foreach (XmlElement element in sl)
+                {
+                    season.Series.Add(new Series()
+                    {
+                        SeriesLink = element.GetAttribute("href"),
+                        Title = element.InnerText
+                    });
+                }
+            }
+
+            return seasons;
+        }
+
+        static async Task<SeasonInfo> GetSeasonInfo(HttpClient client, string url)
+        {
+            var xml = await GetXml(client, url);
+            // todo wichtig für title image und co
+            var seriesInfo = xml.SelectNodes("//*[@id='series']");
+
+            var image = seriesInfo.Item(0).SelectSingleNode("//*[@class='backdrop']") as XmlElement;
+
+            var title = seriesInfo.Item(0).SelectSingleNode("//*[@class='series-title']/h1/span");
+
+            var des = seriesInfo.Item(0).SelectSingleNode("//*[@class='seri_des']");
+
+            var imageString = image != null ? image.GetAttribute("style") : "";
+
+            var imageFrom = imageString.IndexOf("url(") + 4;
+            var imageResult = imageString.Substring(imageFrom);
+            var result = imageResult.Substring(0, imageResult.Length - 1);
+            
+
+            return new SeasonInfo()
+            {
+                Title = title.InnerText,
+                Description = des.InnerText,
+                Image = result
+            };
+        }
+
+        static async Task<SeasonInfo> GetAllFromLink(HttpClient client, string url)
+        {
+            var info = await GetSeasonInfo(client, url);
+            var movie = await GetAllSeason(client, url);
+            info.Seasons = movie;
+            return info;
+        }
     }
+    
 }
---
+++ src/Dummy/Season.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dummy
+{
+    public class Season
+    {
+        public string SeasonTitle { get; set; }
+        public string SeasonLink { get; set; }
+        public List<Series> Series { get; set; } = new List<Series>();
+    }
+
+    public class Series
+    {
+        public string Title { get; set; }
+        public string SeriesLink { get; set; }
+    }
+
+    public class SeasonInfo
+    {
+        public string Image { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public List<Season> Seasons { get; set; }
+    }
+}
